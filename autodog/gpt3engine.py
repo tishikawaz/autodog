6626fe1618503fa027ac58890244494ad3e746bc
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2023-06-05 00:10:16 +0900

    Add genarate methods

--- autodog/gpt3engine.py
+++ autodog/gpt3engine.py
@@ -1,9 +1,12 @@
 from .docengine import DocEngine
+import openai
 
 class GPT3Engine(DocEngine):
-    def __init__(self, notes=[], doc_type='docstring', *args, **kwargs):
+    def __init__(self, notes=[], doc_type='docstring', api_key='', engine="davinci", *args, **kwargs):
         self.notes = notes
         self.doc_type = doc_type
+        self.engine = engine
+        openai.api_key=api_key
 
     def _make_question(self, code:str, lang='') -> str:
         q = 'Please suggest a {0} for the following {1} code and write the {0} only.'.format(
@@ -17,3 +20,39 @@ class GPT3Engine(DocEngine):
         for note in self.notes:
             q + '- {0}'.format(note)
         return q
+
+    def generate_code_doc(self, code) -> str:
+        prompt = self._make_question(code)
+        response = openai.Completion.create(engine      = self.engine,
+                                            prompt      = prompt,
+                                            max_tokens  = 100,
+                                            temperature = 0.5,
+                                            echo        = False)
+        return response["choices"][0]['text']
+
+    def generate_module_doc(self, code) -> str:
+        prompt = self._make_question(code)
+        response = openai.Completion.create(engine      = self.engine,
+                                            prompt      = prompt,
+                                            max_tokens  = 100,
+                                            temperature = 0.5,
+                                            echo        = False)
+        return response["choices"][0]['text']
+
+    def generate_class_doc(self, code) -> str:
+        prompt = self._make_question(code)
+        response = openai.Completion.create(engine      = self.engine,
+                                            prompt      = prompt,
+                                            max_tokens  = 100,
+                                            temperature = 0.5,
+                                            echo        = False)
+        return response["choices"][0]['text']
+
+    def generate_func_doc(self, code) -> str:
+        prompt = self._make_question(code)
+        response = openai.Completion.create(engine      = self.engine,
+                                            prompt      = prompt,
+                                            max_tokens  = 100,
+                                            temperature = 0.5,
+                                            echo        = False)
+        return response["choices"][0]['text']
