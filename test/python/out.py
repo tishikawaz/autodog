dab8131704fe2755082ae46037c38fd0b3fbcae3
Tatsumasa Ishikawa <tatsumasa.ishikawa@ishsoftware.jp>
2023-07-30 00:56:46 +0900

    Update test

--- test/python/out.py
+++ test/python/out.py
@@ -1,7 +1,9 @@
-"""Python module for working with Fortran code
-This module provides a class `FortranCode` that allows you to read and manipulate Fortran code files. It uses the `FortranAST` class from the `ast.fortran` module to parse the code and create an abstract syntax tree (AST). The AST can then be converted back to a string representation of the code.
-Todo:
-- None
+"""FortranCode module
+This module provides a class for working with Fortran code files. It
+allows parsing and manipulating Fortran code using the FortranAST class
+from the ast.fortran module. The parsed code can be converted to a
+string representation and written to a file.
+Todo: None
 """
 from .ast.fortran import FortranAST, ModuleNode, TypeNode, FunctionNode, SubroutineNode, ProgramNode
 from .docengine import DocEngine
@@ -12,19 +14,26 @@ class FortranCode:
     This class represents a Fortran code file.
     Attributes:
         filepath (str): The path to the Fortran code file.
-        tree (FortranAST): The abstract syntax tree of the Fortran code.
+        tree (FortranAST): The abstract syntax tree representation of the
+        Fortran code.
     Methods:
-        - __init__(self, filepath: str): Initializes the FortranCode object with the given filepath. It also reads the file and creates the abstract syntax tree.
-        - to_str(self) -> str: Converts the abstract syntax tree to a string representation.
-        - write(self, filepath) -> None: Writes the string representation of the abstract syntax tree to the specified filepath.
-        - write(self) -> None: Writes the string representation of the abstract syntax tree to the original filepath.
+        __init__(self, filepath: str): Initializes a FortranCode object with
+        the given filepath. It also reads the file and creates the abstract
+        syntax tree.
+        to_str(self) -> str: Converts the abstract syntax tree to a string
+        representation.
+        write(self, filepath) -> None: Writes the string representation of
+        the abstract syntax tree to the specified filepath.
+        write(self) -> None: Writes the string representation of the
+        abstract syntax tree to the original filepath.
     """
 
     def __init__(self, filepath: str):
         """__init__
-        This function initializes an instance of a class and reads a Fortran file to create an abstract syntax tree.
+        This function initializes an instance of a class.
         Args:
-            filepath (str): The path to the Fortran file.
+            filepath (str): The path to the file that will be used to initialize
+            the instance.
         Returns:
             None
         Raises:
@@ -43,23 +52,23 @@ class FortranCode:
             self.tree = FortranAST(f.read())
 
     def to_str(self) -> str:
-        """one-line description:
-        Converts the tree to a string representation.
-        Description:
-        This function converts the tree object to a string representation using the `to_str()` method of the tree.
+        """to_str
+        This function converts the tree object to a string representation.
         Args:
             None
         Returns:
-            str: The string representation of the tree.
+            str : The string representation of the tree.
         Raises:
             None
         Yields:
             None
         Examples:
             ```
-            tree = Tree()
-            tree_str = tree.to_str()
-            print(tree_str)
+            tree = BinaryTree()
+            tree.insert(5)
+            tree.insert(3)
+            tree.insert(7)
+            print(tree.to_str())  # Output: "5, 3, 7"
             ```
         Note:
             None
@@ -68,52 +77,51 @@ class FortranCode:
 
     @singledispatchmethod
     def write(self, filepath) -> None:
-        '''```
-        @staticmethod
-        def write(filepath: str) -> None:
-            """
-            Writes the content of the object to a file.
-            Args:
-                filepath (str): The path of the file to write.
-            Returns:
-                None
-            Raises:
-                FileNotFoundError: If the specified file path does not exist.
-            Examples:
-                obj = MyClass()
-                obj.write('output.txt')
-            Note:
-                - This function will overwrite the content of the file if it already exists.
-                - If the file does not exist, it will be created.
-            """
-            with open(filepath, 'w') as f:
-                f.write(self.to_str())
-        ```
-        '''
+        """Writes the content of an object to a file.
+        This function takes a filepath as an argument and writes the string
+        representation of the object to the file specified by the filepath. The
+        file is opened in write mode, and if the file already exists, its
+        contents will be overwritten.
+        Args:
+            filepath (str): The path to the file where the object will be
+            written.
+        Returns:
+            None: This function does not return any value.
+        Raises:
+            None: This function does not raise any exceptions.
+        Examples:
+            ```
+            obj = MyClass()
+            obj.write('output.txt')
+            ```
+        Note:
+            This function assumes that the object has a `to_str()` method that
+            returns a string representation of the object.
+        """
         with open(filepath, 'w') as f:
             f.write(self.to_str())
 
     @write.register
     def write(self) -> None:
-        """one-line description:
-        Writes the string representation of an object to a file.
-        Description:
-        This function opens a file in write mode and writes the string representation of an object to the file.
+        """Writes the content of the object to a file.
+        This function opens the file specified by `self.filepath` in write mode
+        and writes the string representation of the object to the file.
         Args:
-            None
+            None.
         Returns:
-            None
+            None.
         Raises:
-            None
+            None.
         Yields:
-            None
+            None.
         Examples:
             ```
             obj = MyClass()
             obj.write()
             ```
         Note:
-            None
+            This function will overwrite the contents of the file if it already
+            exists.
         """
         with open(self.filepath, 'w') as f:
             f.write(self.to_str())
